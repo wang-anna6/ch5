9c61fb65808ebb1597ec453a418a5ed9b8e3ec3e
wang-anna6 <a6046577755@gmail.com>
2025-10-03 10:56:42 -0700

    Update Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -4,12 +4,17 @@ public class Quadratic {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Input a: ");
+        validate(in);
         int a = in.nextInt();
         System.out.println("Input b: ");
+        validate(in);
         int b = in.nextInt();
         System.out.println("Input c: ");
+        validate(in);
         int c = in.nextInt();
+        
         int dis = discriminantcheck(a,b,c);
+        
         //checking for erros
         if (a == 0) {
             //dividing by zero
@@ -33,4 +38,9 @@ public class Quadratic {
         return 1;
     }
     
+    public static void validate(Scanner a) {
+        if (!a.hasNextDouble()) {
+            System.err.println(a.next() + " is not a number.");
+        }
+    }
 }
